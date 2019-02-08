23514aab1d0434bdce0f10d77da4cd292b175584
Sönke <me@eknoes.de>
2019-02-08 21:45:42 +0100

    Upgrade dependencies, version bump

---
+++ app/src/test/java/de/eknoes/inofficialgolem/updater/feedParserTest.java
@@ -0,0 +1,20 @@
+package de.eknoes.inofficialgolem.updater;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class feedParserTest {
+
+    static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+    @Test
+    public void getItems() {
+        // Test Invalid Feed
+    }
+}
